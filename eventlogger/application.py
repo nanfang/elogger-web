56d3c5f5396e6d6091b8c1fb2de1042ae9971f8e
Fang Nan <nanfang05@gmail.com>
2011-03-04 23:38:44 +0800

    get event log to work

--- eventlogger/application.py
+++ eventlogger/application.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from eventlogger.database import redis
+from flask.helpers import jsonify
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -16,6 +17,16 @@ def tasks():
 @app.route('/event-log')
 def event_log():
     return render_template('event-log.html')
+
+@app.route('/event-log/<int:year>')
+def annual_log(year):
+    return jsonify(redis.hgetall('event-log:%s' % year))
+
+@app.route('/event-log/<int:year>', methods=['POST', 'PUT'])
+def save_log(year):
+    print(request.form)
+    redis.hset('event-log:%s' % year, request.form['element_id'] , request.form['update_value'] )
+    return '<pre>'+request.form['update_value'] + '</pre>'
 
 @app.route('/tasks', methods=['POST'])
 def save_tasks():
