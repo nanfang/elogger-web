29cb7474c85210173dcdcfd6a789c62873a70e58
nanfang <nanfang@localhost.(none)>
2011-05-17 08:55:01 +0800

    change 'task list' to 'notes' as title

--- elogger/runserver.py
+++ elogger/runserver.py
@@ -4,9 +4,8 @@ from elogger.database import redis
 
 @app.context_processor
 def inject_direction():
-    direction=redis.get('direction:'+session['username'])
+    direction=redis.get('direction:'+session.get('username', 'add your current direction here'))
     return dict(direction=direction)
 
 app.secret_key = 'hello'
 app.run(debug=True)
-
