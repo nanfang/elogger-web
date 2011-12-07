813d7daf4aa00c76b3b6295d03f3c53dc603e184
Fang Nan <nanfang05@gmail.com>
2011-12-07 17:45:23 +0800

    auth api call by look up auth user

--- api/auth.py
+++ api/auth.py
@@ -1,26 +1,28 @@
 import base64
+import keys
 
 def basic_auth(func):
     def callf(webappRequest, *args, **kwargs):
-        # Parse the header to extract a user/password combo.
-        # We're expecting something like "Basic XZxgZRTpbjpvcGVuIHYlc4FkZQ=="
-        auth_header = webappRequest.request.headers.get('Authorization')
-
-        if auth_header == None:
-            webappRequest.response.set_status(401, message="Authorization Required")
-            webappRequest.response.headers['WWW-Authenticate'] = 'Basic realm="eLogger"'
-        else:
-            # Isolate the encoded user/passwd and decode it
-            auth_parts = auth_header.split(' ')
-            user_pass_parts = base64.b64decode(auth_parts[1]).split(':')
-            user_arg = user_pass_parts[0]
-            pass_arg = user_pass_parts[1]
-
-            # TODO use user name and api_key
-            if user_arg != "elogger" or pass_arg != "nanfang":
-                webappRequest.response.set_status(401, message="Authorization Required")
-                webappRequest.response.headers['WWW-Authenticate'] = 'Basic realm="eLogger"'
-            else:
-                return func(webappRequest, *args, **kwargs)
-
+        if auth_user(webappRequest):
+            return func(webappRequest, *args, **kwargs)
+        webappRequest.response.set_status(401, message="Authorization Required")
+        webappRequest.response.headers['WWW-Authenticate'] = 'Basic realm="eLogger"'
     return callf
+
+def auth_user(webappRequest):
+    auth_header = webappRequest.request.headers.get('Authorization')
+    if not auth_header:
+        return False
+    
+    auth_parts = auth_header.split(' ')
+    user_pass_parts = base64.b64decode(auth_parts[1]).split(':')
+    username = user_pass_parts[0]
+    api_key = user_pass_parts[1]
+    user = AuthUser.get_by_key_name(username)
+
+    if not user or (api_key != user.api_key and api_key != keys.master_key):
+        return False
+    
+    webappRequest.user = user
+    
+    return True
