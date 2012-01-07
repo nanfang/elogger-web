6564bda053b27b2c4a1d985eb75a56d898a41a74
Fang Nan <nanfang05@gmail.com>
2012-01-07 21:05:51 +0800

    fix merge

--- elogger/views.py
+++ elogger/views.py
@@ -148,7 +148,7 @@ class DayLogHandler(BaseHandler):
         year = self.get_argument("year", None)
         month = self.get_argument("month", None)
         monthNum = self.get_argument("monthNum", 1)
-        integration.get_month_logs(user['username'], year, month, monthNum, callback=self._on_get_logs)
+        integration.get_month_logs(user['username'], year, month, callback=self._on_get_logs)
 
     def _on_get_logs(self, day_logs):
         self.set_status(200)
