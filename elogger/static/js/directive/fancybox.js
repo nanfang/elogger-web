6151a662c39374cb43944812baf9feaff803c93e
Fang Nan <nanfang05@gmail.com>
2012-01-15 14:53:21 +0800

    add some fancy style, enjoy

--- elogger/static/js/directive/fancybox.js
+++ elogger/static/js/directive/fancybox.js
@@ -1,25 +1,25 @@
-angular.directive('my:fancybox', function(expression, compiledElement) {
+angular.directive('my:fancybox', function (expression, compiledElement) {
     var compiler = this;
-    return function(linkElement) {
+    return function (linkElement) {
         var currentScope = this;
         var log = currentScope.log;
         $(linkElement).fancybox({
-                title: "修改记录",
-                maxWidth    : 800,
-                maxHeight    : 600,
-                fitToView    : false,
-                width        : '70%',
-                height        : '70%',
-                autoSize    : false,
-                closeClick    : false,
-                openEffect    : 'none',
-                closeEffect    : 'none',
-                beforeLoad: function() {
-                    this.title = log.title;
-                },
-                afterClose: function() {
-                    currentScope.saveLog(log);
-                }
-            });
+            maxWidth:732,
+            maxHeight:423,
+            padding:0,
+            fitToView:false,
+            width:'100%',
+            height:'100%',
+            autoSize:false,
+            closeClick:false,
+            openEffect:'elastic',
+            closeEffect:'fade',
+            beforeLoad:function () {
+                this.title = log.title;
+            },
+            afterClose:function () {
+                currentScope.saveLog(log);
+            }
+        });
     };
 });
